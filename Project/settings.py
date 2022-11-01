1a40f144a06ebca77bc0cb86cead5066460dd824
Uchechukwu Anachuna <anachunauchechukwu@gmail.com>
2022-11-01 12:06:01 +0100

    host

--- Project/settings.py
+++ Project/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
