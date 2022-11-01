558bc6707a351ab248cf199730a820e31cf238cf
Uchechukwu Anachuna <anachunauchechukwu@gmail.com>
2022-11-01 13:01:57 +0100

    host

--- account/api/views.py
+++ account/api/views.py
@@ -1,11 +1,12 @@
-from .serializers import UserRegistrationSerializer
+from .serializers import UserRegistrationSerializer, LoginSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import authenticate
 from django.contrib import messages
 from django.shortcuts import render, HttpResponse, redirect
-
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.signals import user_logged_in
 
 
 
@@ -42,5 +43,53 @@ def user_login(request):
         #     return redirect('home')
 
 
+@api_view(['POST'])
+def user_login(request):
+    if request.method == "POST":
+        
+        serializer = LoginSerializer(data=request.data)
+        
+        serializer.is_valid(raise_exception=True)
+        
+        user = authenticate(request, username = serializer.validated_data['username'], password = serializer.validated_data['password'])
+        if user:
+            if user.is_active:
+                try:
+                    refresh = RefreshToken.for_user(user)
+                    
+                    user_details = {}
+                    user_details['id']   = user.id
+                    user_details['username'] = user.username
+                    user_details['email'] = user.email
+                    user_details['refresh_token'] = str(refresh)
+                    user_details['access_token'] = str(refresh.access_token)
+                    user_logged_in.send(sender=user.__class__,
+                                        request=request, user=user)
+
+                    data = {
+                    'message' : "success",
+                    'data' : user_details,
+                    }
+                    return Response(data, status=status.HTTP_200_OK)
+
+
+                except Exception as e:
+                    raise e
+            
+            else:
+                data = {
+                    'message'  : "failed",
+                    'errors': 'This account is not active'
+                    }
+                return Response(data, status=status.HTTP_403_FORBIDDEN)
+
+
+        else:
+            data = {
+                'message'  : "failed",
+                'errors': 'Please provide a valid username and password'
+                }
+            return Response(data, status=status.HTTP_401_UNAUTHORIZED)
+    
 
 
